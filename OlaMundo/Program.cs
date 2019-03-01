a1dcbccdebe2da8c2700db90f128a536aa7783ae
Freeze88 <andrevitorino1997@hotmail.com>
2019-03-01 12:42:54 +0000

    organized the comments and added one to the parameter

--- OlaMundo/Program.cs
+++ OlaMundo/Program.cs
@@ -4,19 +4,26 @@ namespace OlaMundo
 {
     /// <summary>
     /// 
-    /// Simple program design to introduce me to C#.
-    /// Simply writes 2 lines "Hello World!" and "Bye World!"
-    /// 
-    /// in the end checks for an input to keep the window open while the user
-    /// wants.
-    /// 
-    /// It also for now takes arguments in even tho it doesn't do anything 
-    /// right now
-    /// 
+    ///The program starts here. 
+    ///
     /// </summary>
     
     class Program
     {
+        /// <summary>
+        /// Simple program design to introduce me to C#.
+        /// Simply writes 2 lines "Hello World!" and "Bye World!"
+        /// 
+        /// in the end checks for an input to keep the window open while the user
+        /// wants.
+        /// 
+        /// </summary>
+        /// 
+        /// <param name="args">
+        /// It also for now takes arguments in even tho it doesn't do anything 
+        /// right now
+        /// 
+        /// </param>
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
