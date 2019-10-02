689fe54b82220754441d2bd1964c2122a208b821
André Vitorino <andrevitorino1997@hotmail.com>
2019-10-02 16:48:05 +0100

    First commit, add new program of a small game

---
+++ TiposVarios/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TiposVarios
+{
+    class Program
+    {
+        static void Main()
+        {
+            byte a = 10;
+            sbyte b = 20;
+            short c = 30;
+            ushort d = 40;
+            int e = 50;
+            uint f = 60;
+            long g = 70;
+            ulong h = 80;
+            char i = '\u00C6';
+
+            Console.Write("{0},{1},{2},{3},{4},{5},{6},{7},{8}", a, b, c, d, e, f, g, h, i);
+
+
+            Console.WriteLine("\n Hello World!");
+
+            Console.Read();
+        }
+    }
+}
